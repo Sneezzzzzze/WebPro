cf60582409728e792951dc0c238d1633a85f72cd
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2024-02-20 13:58:39 +0700

    fix

--- project/routes/web.php
+++ project/routes/web.php
@@ -14,14 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('welcome');
-});
-=======
-    return view('test');
 });
 
 // Route::get('/test', function () {
 //     return view('test');
 // });
->>>>>>> main
