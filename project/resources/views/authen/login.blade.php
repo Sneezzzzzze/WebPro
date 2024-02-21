5ea1f49c74dd7c44955d5c8540d1e05b9a8ac8a6
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2024-02-21 21:06:50 +0700

    Squashed commit of the following:
    commit be69ef823ab32e656049950a76759fc4bf94088d
    Author: Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
    Date:   Wed Feb 21 08:53:59 2024 +0700
    
        login modify background
    
    commit 598048702ef0f0a603ac2d6979c204697e573820
    Author: Sweet eyes <65070047@kmitl.ac.th>
    Date:   Wed Feb 21 00:30:09 2024 +0700
    
        login page
    
        ทำแค่ตัว view
    
    commit 77c901ab0c6a26fcae5795ba765b4faf4513e713
    Author: Sweet eyes <65070047@kmitl.ac.th>
    Date:   Tue Feb 20 22:09:03 2024 +0700
    
        authen
    
    commit 675efdfc3886adf9e8c66fb40a4ae831f7490d28
    Author: Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
    Date:   Tue Feb 20 13:48:23 2024 +0700
    
        Update web.php
    
    commit 2f40ccce83a4f24b470fc4e3c768782b98525e50
    Author: Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
    Date:   Tue Feb 20 11:53:26 2024 +0700
    
        test laravel
    
    commit a5af462c9bd01718146c162dc1d8b0e5622e301f
    Author: Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
    Date:   Tue Feb 20 11:17:39 2024 +0700
    
        Squashed commit of the following:
    
        commit 2e2c6230c92bc03a7eaeb990245257f1a1319377
        Author: Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
        Date:   Tue Feb 20 11:17:10 2024 +0700
    
            laravel again

---
+++ project/resources/views/authen/login.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body {
+            background: #5C5470;
+        }
+
+        .card {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 400px;
+            height: 440px;
+            margin: 0 auto;
+            margin-top: 220px;
+            border-radius: 10%;
+            backdrop-filter: blur(15px);
+            background-color: rgba(255, 255, 255, 0.5);
+        }
+
+        .card-body {
+            margin-top: 35px;
+        }
+
+        .icon {
+            display: flex;
+            justify-content: center;
+            margin-top: 10px;
+            margin-bottom: 20px;
+        }
+
+        .btn-primary {
+            width: 7vw;
+            height: 5vh;
+        }
+
+        span {
+            margin-top: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="black" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                    </svg>
+                </div>
+                <h4 class="card-title text-center mb-4">Login</h4>
+                <form action="">
+                    <div class="mb-3">
+                        <input type="email" class="form-control" placeholder="Email" required>
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" placeholder="Password" required>
+                    </div>
+                    <span class="d-flex justify-content-center gap-2">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                        <button type="button" class="btn btn-secondary">Create account</button>
+                    </span>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- project/routes/web.php
+++ project/routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('test');
 });
 
-// Route::get('/test', function () {
-//     return view('test');
-// });
+Route::get('/login', function () {
+    return view('authen/login');
+});
