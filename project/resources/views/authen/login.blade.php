be69ef823ab32e656049950a76759fc4bf94088d
Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
2024-02-21 08:53:59 +0700

    login modify background

--- project/resources/views/authen/login.blade.php
+++ project/resources/views/authen/login.blade.php
@@ -8,8 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         body {
-            background: rgb(223, 188, 94);
-            background: radial-gradient(circle, rgba(223, 188, 94, 1) 0%, rgba(215, 60, 55, 1) 100%);
+            background: #5C5470;
         }
 
         .card {
@@ -23,7 +22,7 @@
             margin-top: 220px;
             border-radius: 10%;
             backdrop-filter: blur(15px);
-            background-color: rgba(255, 255, 255, 0.2);
+            background-color: rgba(255, 255, 255, 0.5);
         }
 
         .card-body {
@@ -53,7 +52,7 @@
         <div class="card">
             <div class="card-body">
                 <div class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="black" class="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
                     </svg>
