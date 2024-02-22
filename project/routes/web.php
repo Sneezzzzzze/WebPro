72bcfba5d73b6aac72873e6841c34339c6577b2a
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2024-02-22 14:42:41 +0700

    add loginController and connect to mysql server

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,25 +22,5 @@ Route::get('/login', function () {
     return view('authen/login');
 });
 
-Route::post('/login', function () {
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "sunna1972";
-    $dbname = "webpro";
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $sql = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
-    $result = mysqli_query($conn, $sql);
-    if ($result && mysqli_num_rows($result) > 0) {
-        echo "Login success";
-    } else {
-        echo "Login failed";
-    }
-
-    mysqli_close($conn);
-});
+Route::post('/login', [LoginController::class, 'login']);
