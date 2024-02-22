6024967a5eae6a3bdbc15977b1eb5bc31762d6df
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2024-02-22 14:47:55 +0700

    Squashed commit of the following:
    commit d299f15316491f72c7bb3bf70c8a77c12bcf54e6
    Author: Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
    Date:   Thu Feb 22 14:46:46 2024 +0700
    
        login
    
        แก้ email เป็น text
    
    commit a7176b72bc79d0fbdb8cf8cba9d05c2fe6411465
    Author: Sweet eyes <65070047@kmitl.ac.th>
    Date:   Wed Feb 21 23:23:40 2024 +0700
    
        register finish
    
    commit 298bd3e665880efc754f489fb334cc101366b7d5
    Author: Sweet eyes <65070047@kmitl.ac.th>
    Date:   Wed Feb 21 22:58:52 2024 +0700
    
        register not finish
    
    commit 6e300c1aea46606e4ca9ed1d6906ab4bbe260602
    Merge: be69ef8 937274d
    Author: Sweet eyes <65070047@kmitl.ac.th>
    Date:   Wed Feb 21 22:25:35 2024 +0700
    
        Merge remote-tracking branch 'origin/Junbeom' into BigBoss
    
    commit be69ef823ab32e656049950a76759fc4bf94088d
    Author: Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
    Date:   Wed Feb 21 08:53:59 2024 +0700
    
        login modify background
    
    commit 598048702ef0f0a603ac2d6979c204697e573820
    Author: Sweet eyes <65070047@kmitl.ac.th>
    Date:   Wed Feb 21 00:30:09 2024 +0700
    
        login page
    
        ทำแค่ตัว view
    
    commit 77c901ab0c6a26fcae5795ba765b4faf4513e713
    Author: Sweet eyes <65070047@kmitl.ac.th>
    Date:   Tue Feb 20 22:09:03 2024 +0700
    
        authen
    
    commit 675efdfc3886adf9e8c66fb40a4ae831f7490d28
    Author: Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
    Date:   Tue Feb 20 13:48:23 2024 +0700
    
        Update web.php
    
    commit 2f40ccce83a4f24b470fc4e3c768782b98525e50
    Author: Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
    Date:   Tue Feb 20 11:53:26 2024 +0700
    
        test laravel
    
    commit a5af462c9bd01718146c162dc1d8b0e5622e301f
    Author: Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
    Date:   Tue Feb 20 11:17:39 2024 +0700
    
        Squashed commit of the following:
    
        commit 2e2c6230c92bc03a7eaeb990245257f1a1319377
        Author: Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
        Date:   Tue Feb 20 11:17:10 2024 +0700
    
            laravel again

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +21,29 @@ Route::get('/login', function () {
     return view('authen/login');
 });
 
+Route::post('/login', function () {
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "sunna1972";
+    $dbname = "webpro";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-Route::post('/login', [LoginController::class, 'login']);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
+    if ($result && mysqli_num_rows($result) > 0) {
+        echo "Login success";
+    } else {
+        echo "Login failed";
+    }
+
+    mysqli_close($conn);
+});
+
+Route::get('/register', function () {
+    return view('authen/register');
+});
