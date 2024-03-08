c8c18b3ab888b2a9c79bdfc134552af2c6195616
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2024-03-08 21:40:58 +0700

    add InCart and fix quntity

--- project/resources/views/food_page/status.blade.php
+++ project/resources/views/food_page/status.blade.php
@@ -8,6 +8,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ URL::asset('css/food_page/status_style.css'); }}">
     <meta http-equiv="refresh" content="10">
+    <script>
+        window.history.pushState(null, "", window.location.href);
+        window.onpopstate = function() {
+            window.history.pushState(null, "", window.location.href);
+        };
+    </script>
 </head>
 
 <body background="https://www.it.kmitl.ac.th/~pattarachai/PIC/BG/stone.gif" link="#0000BB">
@@ -38,10 +44,10 @@
         ->orderBy('time', 'asc')
         ->limit($count)
         ->get();
+    
     $allDone = true;
-
     foreach ($leastValues as $value) {
-        if ($value->status != 'เสร็จแล้ว') {
+        if ($value->status !== 'เสร็จแล้ว') {
             $allDone = false;
             break;
         }
