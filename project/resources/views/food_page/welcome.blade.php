af13826327ade1e42082d798b764dead52992c94
Chirakit <65070034@kmitl.ac.th>
2024-03-11 10:14:22 +0700

    Typo

--- project/resources/views/food_page/welcome.blade.php
+++ project/resources/views/food_page/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ร้านติ่มซำ ยินดีย้อนรับ</title>
+    <title>ร้านติ่มซำ ยินดีต้อนรับ</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="icon" href="{{ URL::asset('image/DimsumLogo.png')}}" type="image/x-icon">
